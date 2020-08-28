096f2053f88e0a75bf69f4416cd489294e9bb8d4
progfay <shun1856shun@gmail.com>
2020-08-28 23:47:05 +0900

    doc: add godoc

--- interval/interval.go
+++ interval/interval.go
@@ -5,12 +5,16 @@ import (
 	"time"
 )
 
+// SetInterval calls a function repeatedly with a fixed time delay between each call.
+// To stopping repeatedly function calling, call returned function.
 func SetInterval(function func(), interval time.Duration) (cancel func()) {
 	ctx, cancel := context.WithCancel(context.Background())
 	SetIntervalWithContext(ctx, function, interval)
 	return cancel
 }
 
+// SetIntervalWithContext calls a function repeatedly with a fixed time delay between each call.
+// When ctx is cancelled, repeatedly function calling is cancelled.
 func SetIntervalWithContext(ctx context.Context, function func(), interval time.Duration) {
 	go func(f func(), i time.Duration) {
 		for {
